b7b16b0e2625a99d7df579591f0bd0e4df744843
kamjan60 <kamjan60@wp.pl>
2020-04-06 19:46:22 +0200

    Modified main class to get authors names.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(System.getProperty("java.version"));
-        String project_name="laboratorium01";
+        DEV1 developer1 = new DEV1();
+        DEV2 developer2 = new DEV2();
+        System.out.println(developer1.author);
+        System.out.println(developer2.author);
     }
 }

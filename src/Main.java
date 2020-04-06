b4e35e7c231d03c5210105a9cc83728b5af1ff01
kamjan60 <kamjan60@wp.pl>
2020-04-06 19:32:27 +0200

    Added project name to the main class.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(System.getProperty("java.version"));
+        String project_name="laboratorium01";
     }
 }
